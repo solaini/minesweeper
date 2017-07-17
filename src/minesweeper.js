573ba28c5fbd45e2934543901183e52ef44c7261
Alexander Coffin <alexandercoffin@alexanders-mbp.attlocal.net>
2017-07-17 13:47:32 -0500

    Dynamically generating the bomb board now

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,17 +1,51 @@
 //Function prints the current status of the Minesweeper board
-const printBoard = () => {
-    console.log("Current Board:");
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
+// const printBoard = () => {
+//     console.log("Current Board:");
+//     console.log(board[0].join(' | '));
+//     console.log(board[1].join(' | '));
+//     console.log(board[2].join(' | '));
+// }
+
+
+const printBoard = board => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
 }
-//Empty game board array
-const board = [ 
-    [' ', ' ', ' '], 
-    [' ', ' ', ' '], 
-    [' ', ' ', ' ']
-];
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+
+//Generate the player board
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+    for(let i = 0; i < numberOfRows; i++){
+        let row = [];
+        for(j = 0; j < numberOfColumns; j++){
+            row.push(' ');
+        } 
+        board.push(row);
+    }
+    return board
+}
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for(let i = 0; i < numberOfRows; i++){
+        let row = [];
+        for(j = 0; j < numberOfColumns; j++){
+            row.push(null);
+        } 
+        board.push(row);
+    }
+    let numberOfBombsPlaced = 0;
+    while(numberOfBombs > numberOfBombsPlaced){
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+       //Code has potential to duplicate locations for bomb placement.  Will fix this after learning control flow.
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+    }
+    
+    return board;
+}
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
